0dbdbb5a7b253b8c4a5862187694b7740cb078b4
unknown <lssuseendharlal@gmail.com>
2021-01-13 12:47:09 +0530

    register page added

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 const Register = () => {
   return (
@@ -11,6 +12,58 @@ const Register = () => {
         className="w-40 h-screen bg-center bg-cover"
         style={{ backgroundImage: "url('/images/bricks.jpg')" }}
       ></div>
+      <div className="flex flex-col justify-center pl-6">
+        <div className="w-70">
+          <h1 className="mb-2 text-lg font-medium">Sign up</h1>
+          <p className="mb-10 text-xs">
+            By continuing, you agree to our User Agreement and Privacy Policy.
+          </p>
+          <form autoComplete="off">
+            <div className="mb-6">
+              <input
+                type="checkbox"
+                className="mr-1 cursor-pointer"
+                id="agreement"
+              />
+              <label htmlFor="agreement" className="text-xs cursor-pointer">
+                I agree to get emails about cool stuffs on Reddit.
+              </label>
+            </div>
+            <div className="mb-2">
+              <input
+                type="email"
+                className="w-full px-3 py-2 bg-gray-200 border border-gray-300 rounded"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-2">
+              <input
+                autoComplete="off"
+                type="text"
+                className="w-full px-3 py-2 bg-gray-200 border border-gray-300 rounded"
+                placeholder="Username"
+              />
+            </div>
+            <div className="mb-2">
+              <input
+                autoComplete="off"
+                type="password"
+                className="w-full px-3 py-2 bg-gray-200 border border-gray-300 rounded"
+                placeholder="Password"
+              />
+            </div>
+            <button className="w-full py-2 mb-4 text-xs font-bold text-white uppercase bg-blue-500 border-blue-500 rounded">
+              Sign up
+            </button>
+          </form>
+          <small>
+            Already a redditor?
+            <Link href="/login">
+              <a className="ml-1 font-bold text-blue-500 uppercase">Log in</a>
+            </Link>
+          </small>
+        </div>
+      </div>
     </div>
   );
 };
