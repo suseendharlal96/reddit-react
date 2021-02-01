bc31cdd5b11b5b69ffca5f8a82fdf02590b42d5c
unknown <lssuseendharlal@gmail.com>
2021-02-01 18:38:28 +0530

    token check

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    // credentials: true,
+    credentials: true,
     origin: "*",
     optionsSuccessStatus: 200,
   })
