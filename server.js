d67196f231be94e6e181a35b9a6926308737075a
unknown <lssuseendharlal@gmail.com>
2021-02-01 18:47:03 +0530

    token check

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    // credentials: true,
+    credentials: true,
     origin: "https://reddit-react.vercel.app",
     optionsSuccessStatus: 200,
   })
